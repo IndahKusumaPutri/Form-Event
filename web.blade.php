d3aac0f213cb4953f422d335c2c350dbf5ff62a1
IndahKusumaPutri <106747173+IndahKusumaPutri@users.noreply.github.com>
2022-09-27 08:38:42 +0700

    Create web.blade.php

---
+++ web.blade.php
@@ -0,0 +1,6 @@
+Route::resource('/event','EventController');
+Route::get('/event/create', 'EventController@create');
+Route::post('/event/store', 'EventController@store');
+Route::post('/event/edit/{id_event}', 'EventController@update');
+Route::get('/event/show','EventController@show');
+Route::get('/cetakevent','EventController@cetakEvent');
