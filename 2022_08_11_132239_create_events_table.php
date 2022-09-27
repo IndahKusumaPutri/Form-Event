11e4e4c2a51b7487f2a9730cb71e842cc246d9b1
IndahKusumaPutri <106747173+IndahKusumaPutri@users.noreply.github.com>
2022-09-27 08:40:38 +0700

    Add files via upload

---
+++ 2022_08_11_132239_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id_event');
+            $table->integer('id_layanankesehatan');
+            $table->string('nama_event');
+            $table->date('tanggal');
+            $table->text('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
